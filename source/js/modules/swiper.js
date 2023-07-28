3a33cd41b50ec2a58bbace65b7bf25081510ca29
Anastasiia <soboanastasiya@yandex.ru>
2023-07-28 19:10:02 +0300

    вносит правки

---
+++ source/js/modules/swiper.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.coaches__swiper', {
+  // Optional parameters
+  loop: true,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 5,
+    },
+    1360: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.coaches__swiper-button--next',
+    prevEl: '.coaches__swiper-button--prev',
+  },
+});
+
+const swiperReviews = new Swiper('.reviews__swiper', {
+  // Optional parameters
+  slidesPerView: 1,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews__swiper-button--next',
+    prevEl: '.reviews__swiper-button--prev',
+    disabledClass: 'reviews__button-disabled',
+  },
+});
